d6cd4656f238e4cb0a068378ae03b7f32a71c67e
Danny Chai <chaiduo123@163.com>
2019-05-15 23:13:49 +0800

    Update to my Leetcode

--- normal_order/100.SameTree.py
+++ normal_order/100.SameTree.py
@@ -50,6 +50,7 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+    	# DFS
         def search(root):
             if(root == None):
                 output.append('null')               
@@ -64,6 +65,38 @@ class Solution:
         search(q)
         tree2 = output
         return tree1 == tree2
+        
+# # DFS with stack        
+# def isSameTree2(self, p, q):
+#     stack = [(p, q)]
+#     while stack:
+#         node1, node2 = stack.pop()
+#         if not node1 and not node2:
+#             continue
+#         elif None in [node1, node2]:
+#             return False
+#         else:
+#             if node1.val != node2.val:
+#                 return False
+#             stack.append((node1.right, node2.right))
+#             stack.append((node1.left, node2.left))
+#     return True
+ 
+# # BFS with queue    
+# def isSameTree3(self, p, q):
+#     queue = [(p, q)]
+#     while queue:
+#         node1, node2 = queue.pop(0)
+#         if not node1 and not node2:
+#             continue
+#         elif None in [node1, node2]:
+#             return False
+#         else:
+#             if node1.val != node2.val:
+#                 return False
+#             queue.append((node1.left, node2.left))
+#             queue.append((node1.right, node2.right))
+#     return True
 
 x = TreeNode(1)
 p = x
